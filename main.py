1792fc99fe403e224ec94c46db7264afdbb993a9
Lauri Kananen <lauri.kananen@gmail.com>
2020-06-02 15:24:04 +0300

    Image capture loop created.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep
 import os
 
 def log(msg):
+    # Customized print function
     print("> ",end="")
     print(msg)
 
@@ -18,6 +19,25 @@ def setup_camera():
 
     log("Setup complete!")
     return cam
+
+def capture_img(camera, filename):
+    camera.capture(filename)
+
+def run_snap_loop(camera, s_interval=1, n_tempfiles=2,
+                  save_location=os.getcwd()):
+    tempfile_num_iter = 0
+
+    while True:
+        sleep(s_interval)
+
+        # Take a snapshot
+        filename = os.path.join(save_location, "temp",
+                                "image&s.jpg" %tempfile_num_iter)
+        capture_img(camera, filename)
+
+        # When iterator reaches n it resets to zero
+        tempfile_num_iter = (tempfile_num_iter + 1) % n_tempfiles
+
 
 def main():
     camera = setup_camera()
