ff61aafe98cf72da3f2a3db339aa7ed9caca41c4
Lauri Kananen <lauri.kananen@gmail.com>
2020-06-02 15:02:12 +0300

    Camera initializer created.

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    pass
+import picamera.PiCamera
+from time import sleep
+import os
 
-if __name__ == '__main__':
+def log(msg):
+    print("> ",end="")
+    print(msg)
+
+def setup_camera():
+    # Camera initializer
+    cam = picamera.PiCamera()
+    cam.rotation = 0
+    cam.resolution = (1280, 720)
+    cam.framerate = 30
+    cam.image_effect = 'none'
+    cam.exposure_mode = 'auto'
+    cam.awb_mode = 'auto'
+
+    log("Setup complete!")
+    return cam
+
+def main():
+    camera = setup_camera()
+
+if __name__ == "__main__":
     main()
 
